3702076c89941fd12baaa721a4f155f40359f913
Joshua <goosemonkey97@gmail.com>
2012-08-11 13:15:03 -0700

    #4: Added methods to get FileConfiguration objects of the 3 yml files

--- Scores/src/me/goosemonkey/Scores/Scores.java
+++ Scores/src/me/goosemonkey/Scores/Scores.java
@@ -1,5 +1,6 @@
 package me.goosemonkey.Scores;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Scores extends JavaPlugin
@@ -18,5 +19,32 @@ public class Scores extends JavaPlugin
 		configMain = new ConfigMain(this);
 		configLocale = new ConfigLocale(this);
 		configScores = new ConfigScores(this);
+	}
+	
+	/**
+	 * Get the FileConfiguration object of Config.yml
+	 * @return The main config's FileConfiguration
+	 */
+	public static FileConfiguration getMainConfig()
+	{
+		return Scores.configMain.getConfig();
+	}
+	
+	/**
+	 * Get the FileConfiguration object of Locale.yml
+	 * @return The locale config's FileConfiguration
+	 */
+	public static FileConfiguration getLocaleConfig()
+	{
+		return Scores.configLocale.getConfig();
+	}
+
+	/**
+	 * Get the FileConfiguration object of ScoresData.yml
+	 * @return The score data config's FileConfiguration
+	 */
+	public static FileConfiguration getScoresDataConfig()
+	{
+		return Scores.configScores.getConfig();
 	}
 }
