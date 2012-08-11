c3b98262d7bf80c31936be010c8d9e0dfb14d542
Joshua <goosemonkey97@gmail.com>
2012-08-11 13:20:21 -0700

    #5: Added basic methods to get and set scores

--- Scores/src/me/goosemonkey/Scores/Scores.java
+++ Scores/src/me/goosemonkey/Scores/Scores.java
@@ -1,6 +1,7 @@
 package me.goosemonkey.Scores;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Scores extends JavaPlugin
@@ -46,5 +47,25 @@ public class Scores extends JavaPlugin
 	public static FileConfiguration getScoresDataConfig()
 	{
 		return Scores.configScores.getConfig();
+	}
+	
+	/**
+	 * Gets a player's current score from ScoresData.yml
+	 * @param player Player to get score of
+	 * @return Player's score, 0 if player isn't found
+	 */
+	public static int getScore(Player player)
+	{
+		return Scores.getScoresDataConfig().getInt("scores." + player.getName(), 0);
+	}
+	
+	/**
+	 * Most basic method of setting a player's score, and nothing else.
+	 * @param player Player to set score of
+	 * @param score The score
+	 */
+	public static void setScore(Player player, int score)
+	{
+		Scores.getScoresDataConfig().set("scores." + player.getName(), score);
 	}
 }
